231fbf13071aa8a9ff26b20833d86bbb64b8a4a1
Ryan24Price <illusion165@hotmail.com>
2023-12-11 14:19:43 -0500

    ex6 push

--- Exercise 6/controllers/calculatorController.js
+++ Exercise 6/controllers/calculatorController.js
@@ -1,47 +1,45 @@
-const addNumbers = (req, res) => {
-    let number1 = parseInt(req.query.num1);
-    let number2 = parseInt(req.query.num2);
-    let sum = number1 + number2;
-    console.log(number1, number2);
-  
-    res.status(200).json({ result: sum });
-  };
-  
-  
-  const subtractNumbers = (req, res) => {
-      let number1 = parseInt(req.query.num1);
-      let number2 =parseInt(req.query.num2);
-      let difference = number1 - number2;
-      console.log(number1, number2);
-  
-   res.status(200).json({result: difference})
-  };
-  
-  
-  
-  const multiplyNumbers = (req, res) => {
-      let number1 = parseInt(req.query.num1);
-      let number2 =parseInt(req.query.num2);
-      let product = number1 * number2;
-      console.log(number1, number2);
-  
-       res.status(200).json({result: product});
-  };
-  
-  
-  const divideNumbers = (req, res) => {
-      let number1 = parseInt(req.query.num1);
-      let number2 =parseInt(req.query.num2);
-      let quotient = number1 / number2;
-      console.log(number1, number2);
-  
-      res.status(200).json({result: quotient});
-  };
+const Calculator = require("../libraries/calculator");
 
-  
-  
-  module.exports = {
-      addNumbers, subtractNumbers, multiplyNumbers, divideNumbers,
-  };
-  
+let myCalc = new Calculator();
+
+const addNumbers = (req, res) => {
+  let number1 = parseInt(req.query.num1);
+  let number2 = parseInt(req.query.num2);
+  let sum = myCalc.add(number1, number2);
+  console.log(number1, number2);
+  res.status(200).json({ result: sum });
+};
+
+const subtractNumbers = (req, res) => {
+  let number1 = parseInt(req.query.num1);
+  let number2 = parseInt(req.query.num2);
+  let difference = myCalc.subtract(number1, number2);
+  console.log(number1, number2);
+  res.status(200).json({ result: difference });
+};
+
+const multiplyNumbers = (req, res) => {
+  let number1 = parseInt(req.query.num1);
+  let number2 = parseInt(req.query.num2);
+  let product = myCalc.multiply(number1, number2);
+  console.log(number1, number2);
+  res.status(200).json({ result: product });
+};
+
+const divideNumbers = (req, res) => {
+  let number1 = parseInt(req.query.num1);
+  let number2 = parseInt(req.query.num2);
+  let quotient = myCalc.divide(number1, number2);
+  console.log(number1, number2);
+  res.status(200).json({ result: quotient });
+};
+
+module.exports = {
+  addNumbers,
+  subtractNumbers,
+  multiplyNumbers,
+  divideNumbers,
+};
+
+
   
